7fa78ae35c13c92976844acf7f9c92ade9a4ff4a
Shivang21007 <shivanggupta2611@gmail.com>
2023-09-12 23:57:24 +0530

    vercel.json file created for deployment

--- backend/index.js
+++ backend/index.js
@@ -11,20 +11,21 @@ app.use(express.json());
 
 // Middleware for handling CORS POLICY
 // Option 1: Allow All Origins with Default of cors(*)
-app.use(cors());
+// app.use(cors());
 
 // Option 2: Allow Custom Origins
-// app.use(
-//   cors({
-//     origin: 'http://localhost:5173',
-//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-//     allowedHeaders: ['Content-Type'],
-//   })
-// );
+app.use(
+  cors({
+    origin: 'http://localhost:5173',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type'],
+    credentials: true
+  })
+);
 
 app.get("/", (request, response) => {
   //console.log(request);
-  return response.status(234).send("Welcome To MERN Stack Tutorial");
+  return response.status(234).send("Welcome to Book Store App.");
 });
 
 app.use("/books", booksRoute);
